03324d6337e659d1493dbef722e4b1fe283135dc
Miotis2004 <miotis2004@gmail.com>
2020-12-27 17:08:27 -0800

    Started Registration

--- SocialComponents/SocialComponents/Views/Reg-Login/LoginView.swift
+++ SocialComponents/SocialComponents/Views/Reg-Login/LoginView.swift
@@ -17,29 +17,59 @@ struct LoginView: View {
     @Binding var isLoggedIn: Bool
     
     var body: some View {
-        VStack {
-            Spacer()
-            TextField("Email", text: $email)
-                .padding()
-            Divider()
-            TextField("Password", text: $password)
-                .padding()
-            Button (action: {
-                Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
-                    if error != nil {
-                        self.showAlert = true
-                    } else {
-                        isLoggedIn = true
-                    }
-                    
+        ZStack {
+            Color.blue
+            VStack {
+                Spacer()
+                HStack {
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    TextField("Email", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
                 }
-            }, label: {
-                Text("Login")
-            })
-            .alert(isPresented: $showAlert, content: {
-                Alert(title: Text("Login Error"), message: Text("Something went wrong, try again"), dismissButton: .default(Text("OK")))
-            })
-            Spacer()
+                Divider()
+                HStack {
+                    Image(systemName: "lock.circle")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    TextField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                }
+                Button (action: {
+                    Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+                        if error != nil {
+                            self.showAlert = true
+                        } else {
+                            isLoggedIn = true
+                        }
+                        
+                    }
+                }, label: {
+                    Text("Login")
+                        .foregroundColor(Color.black)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 30)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                })
+                .alert(isPresented: $showAlert, content: {
+                    Alert(title: Text("Login Error"), message: Text("Something went wrong, try again"), dismissButton: .default(Text("OK")))
+                })
+                Button (action: {
+                    RegisterView()
+                }, label: {
+                    Text("Register new account")
+                        .foregroundColor(Color.black)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 30)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                })
+                Spacer()
+            }
         }
     }
 }
---
+++ SocialComponents/SocialComponents/Views/Reg-Login/RegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterView.swift
+//  SocialComponents
+//
+//  Created by Ronald Joubert on 12/27/20.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text("Register")
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
